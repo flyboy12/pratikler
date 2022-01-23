012eac3d759967aafdb70e7b740f6e4e81a0e239
flyboy12 <53239096+flyboy12@users.noreply.github.com>
2022-01-23 22:53:16 +0300

    alıştırma 2

---
+++ degiskenler/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+DateTime dt = DateTime.Now ;
+Console.WriteLine("Günlerden  "+dt.ToString("dddd"));
+string s1 = "20";
+int s2 = 21;
+Console.WriteLine("String işlem : "+s1+s2);
+Console.WriteLine("Matematiksel işlem : "+(int.Parse(s1)+s2));
+Console.WriteLine("Şu an saat :" + dt.ToString("hh:mm"));
+bool bitti = 10>8;
+Console.WriteLine("İşlem Bitmiştir : (ing) "+bitti);
+Console.ReadLine();
