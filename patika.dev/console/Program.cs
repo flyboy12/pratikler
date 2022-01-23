59cfc098977477a75a37189239a87a37537bb65c
flyboy12 <53239096+flyboy12@users.noreply.github.com>
2022-01-23 22:01:16 +0300

    yükleme

---
+++ patika.dev/console/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("İsim : ");
+string? name = Console.ReadLine();
+Console.Write("Soyadı : ");
+string? surname= Console.ReadLine();
+Console.WriteLine(name +" "+ surname);
+Console.ReadLine();
