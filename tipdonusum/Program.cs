88d888d68f8460f15cc97888d5ba05b0dfc98694
flyboy12 <53239096+flyboy12@users.noreply.github.com>
2022-01-26 22:01:58 +0300

    Dönüşümler ve türleri işlenmiştir

---
+++ tipdonusum/Program.cs
@@ -0,0 +1,20 @@
+﻿//Bilinçsiz Dönüşüm (implicit conversion)
+//Düşük kapasiteli bir değikeni yüksek kapasiteli değişkene atamak
+System.Console.WriteLine("***********Bilinçsiz Dönüşüm***********");
+byte a = 5;
+sbyte b = 12;
+int c= a+b;
+string d = "asdasd";
+object e= d+c;
+System.Console.WriteLine("c: "+c);
+System.Console.WriteLine("e: "+e);
+//Bilinçli Dönüşüm(Explicit Conversion)
+//Kendi kendine yapamayacağı dönüşümler olarak düşünülmeli
+System.Console.WriteLine("-------Bilinçli Dönüşüm------");
+int x = 4;
+byte y = (byte) x ;
+System.Console.WriteLine("y: "+y);
+float w = 18.3f;
+y = (byte) w;
+System.Console.WriteLine("y : "+w);//veri kaybı olur
+
