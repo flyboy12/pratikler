a67b16e17afe7313c07c7b1aa89ee0768a4bfe71
flyboy12 <53239096+flyboy12@users.noreply.github.com>
2022-01-26 20:27:55 +0300

    operatörler eklenmiştir

---
+++ operatorler/Program.cs
@@ -0,0 +1,30 @@
+﻿bool islemDevam = true;
+int  sayac=1;
+
+while(islemDevam)
+{
+
+Console.WriteLine(sayac+ ". işleminiz");
+System.Console.WriteLine("----------------------------------");
+Console.WriteLine("1. Sayıyı giriniz: ");
+int s1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("2. Sayıyı giriniz: ");
+int s2 = Convert.ToInt32(Console.ReadLine());
+if(s1 < s2){
+System.Console.WriteLine(s1+" sayısından "+ s2+" sayısı büyüktür");
+}
+else if(s1 > s2){
+System.Console.WriteLine(s2+" sayısından "+ s1+" sayısı büyüktür");
+}
+else{
+    System.Console.WriteLine(s1 +" sayısı "+ s2 +"sayısına eşittir.");
+}
+sayac++;
+System.Console.WriteLine("İşleme devam etmek istiyor musunuz ?");
+
+string? cevap = Console.ReadLine();
+if(cevap!.ToLower() =="hayır")
+      Environment.Exit(0);
+ }
+// See https://aka.ms/new-console-template for more information
+Console.ReadLine();
