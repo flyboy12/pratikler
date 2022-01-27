d1975918207e312cd4946fb2c8902726d505064a
flyboy12 <53239096+flyboy12@users.noreply.github.com>
2022-01-27 13:03:36 +0300

    while foreach örnekleri yapıldı

---
+++ while-foreach/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+char karakter= 'a';
+while (karakter <='z')
+{
+    System.Console.Write( karakter);
+    karakter++;
+    System.Console.Write(" ");
+}
+System.Console.WriteLine();
+string[] arabalar = {"bmw","mercedes","ford"};
+foreach(var item in arabalar){
+System.Console.WriteLine("Araba Markası: "+item);
+}
+Console.ReadLine();
