27e2ce1f429f2dfe1fe9dc8137ceb12f290ae64c
flyboy12 <53239096+flyboy12@users.noreply.github.com>
2022-01-27 12:11:10 +0300

    for döngüsü örneği yapılmıştır

---
+++ for/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+System.Console.WriteLine("Lütfen bir sayı giriniz");
+int? sayac = int.Parse(Console.ReadLine());
+for (int i = 1; i <= sayac; i++)
+{
+    if(i%2==1)
+    {
+     System.Console.WriteLine(i);
+    }
+}
+int tekToplam =0, ciftToplam=0;
+for (int i = 1; i <= sayac; i++)
+{
+    if(i%2==1)
+    {
+      tekToplam +=i;
+    }
+    else
+      ciftToplam+=i;
+}
+
+System.Console.WriteLine("Tek Toplam : "+ tekToplam +"\nÇift Toplam : "+ciftToplam);
+//contineue sadece o işleme uğramamasını devam etmesini tembihler
+// break ise döngüyü durdurur
+Console.ReadLine();
